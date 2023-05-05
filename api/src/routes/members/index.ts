d099ab854fac0198c480d0fa731d756d7dfb0546
Michael Jolley <mike@sparcapp.io>
2023-05-04 21:50:41 -0500

    Moving to API with Deno

--- api/src/routes/members/index.ts
+++ api/src/routes/members/index.ts
@@ -3,7 +3,10 @@ import { Context, Router } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 import { getAllMembers } from "./getAllMembers.ts";
 import { getMember } from "./getMember.ts";
 
-export default new Router()
+const memberRouter = new Router();
+
+memberRouter
     .get("/members", (context: Context) => getAllMembers(context))
     .get("/members/:id", (context: Context) => getMember(context));
 
+export default memberRouter;
