32d0bffc79750da9ee81c2965dc7da59e51baa74
Michael Jolley <mike@sparcapp.io>
2023-05-04 21:10:42 -0500

    Moving to API with Deno

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,10 @@
 import { Application } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
-import { members } from "./routes/members/index.ts";
+import memberRoutes from "./routes/members/index.ts";
 
 const app = new Application();
 
-app.use(members.routes());
-app.use(members.allowedMethods());
+app.use(memberRoutes.routes());
+app.use(memberRoutes.allowedMethods());
 
 await app.listen({ port: 8000, secure: true });
--- api/src/routes/members/index.ts
+++ api/src/routes/members/index.ts
@@ -1,9 +1,6 @@
 import type { Context } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
-const members = new Router();
-
-members
+export default new Router()
     .get("/members", (context: Context) => getAllMembers(context))
     .get("/members/:id", (context: Context) => getMember(context));
 
-export members;
